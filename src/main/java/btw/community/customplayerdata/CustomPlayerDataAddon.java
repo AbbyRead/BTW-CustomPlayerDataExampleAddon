1938197da837c8085c9d5f257bbf66841197acfd
Abigail Read <AbbyReadMail@gmail.com>
2025-11-09 07:02:53 -0800

    Re-indent with tabs to better follow BTW Coding Guidelines

--- src/main/java/btw/community/customplayerdata/CustomPlayerDataAddon.java
+++ src/main/java/btw/community/customplayerdata/CustomPlayerDataAddon.java
@@ -13,38 +13,38 @@ import net.minecraft.src.NetServerHandler;
  */
 public class CustomPlayerDataAddon extends BTWAddon {
 
-    private static CustomPlayerDataAddon instance;
+	private static CustomPlayerDataAddon instance;
 
-    // Player data entry for join count
-    public static final DataEntry.PlayerDataEntry<Integer> JOIN_COUNT_ENTRY =
-            DataProvider.<Integer>getBuilder(Integer.class)
-                    .name("JoinCount")
-                    .defaultSupplier(() -> 0)
-                    .readNBT(nbt -> nbt.hasKey("JoinCount") ? nbt.getInteger("JoinCount") : 0)
-                    .writeNBT((nbt, val) -> nbt.setInteger("JoinCount", val))
-                    .player()           // marks as player-specific
-                    .syncPlayer()       // ensures it syncs with client
-                    .buildPlayer();     // builds the PlayerDataEntry instance
+	// Player data entry for join count
+	public static final DataEntry.PlayerDataEntry<Integer> JOIN_COUNT_ENTRY =
+			DataProvider.getBuilder(Integer.class)
+					.name("JoinCount")
+					.defaultSupplier(() -> 0)
+					.readNBT(nbt -> nbt.hasKey("JoinCount") ? nbt.getInteger("JoinCount") : 0)
+					.writeNBT((nbt, val) -> nbt.setInteger("JoinCount", val))
+					.player()           // marks as player-specific
+					.syncPlayer()       // ensures it syncs with client
+					.buildPlayer();     // builds the PlayerDataEntry instance
 
-    public CustomPlayerDataAddon() {
-        super();
-        instance = this;
-    }
+	public CustomPlayerDataAddon() {
+		super();
+		instance = this;
+	}
 
-    public static CustomPlayerDataAddon getInstance() {
-        return instance;
-    }
+	public static CustomPlayerDataAddon getInstance() {
+		return instance;
+	}
 
-    @Override
-    public void initialize() {
-        AddonHandler.logMessage(this.getName() + " Version " + this.getVersionString() + " Initializing...");
-        // Register the PlayerDataEntry so BTW knows to save it
-        JOIN_COUNT_ENTRY.register();
-    }
+	@Override
+	public void initialize() {
+		AddonHandler.logMessage(this.getName() + " Version " + this.getVersionString() + " Initializing...");
+		// Register the PlayerDataEntry so BTW knows to save it
+		JOIN_COUNT_ENTRY.register();
+	}
 
-    @Override
-    public void serverPlayerConnectionInitialized(NetServerHandler serverHandler, EntityPlayerMP playerMP) {
-        // Delegate join handling entirely to PlayerJoinTracker
-        PlayerJoinTracker.sendWelcomeMessage(playerMP);
-    }
+	@Override
+	public void serverPlayerConnectionInitialized(NetServerHandler serverHandler, EntityPlayerMP playerMP) {
+		// Delegate join handling entirely to PlayerJoinTracker
+		PlayerJoinTracker.sendWelcomeMessage(playerMP);
+	}
 }
